a89e60cdaf7bdb53aa6a816e464e5204dcf5d8d3
Evandro Magalhaes <evandrorm89@gmail.com>
2026-07-18 22:55:35 -0300

    initial commit

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+class HTMLNode:
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: list["HTMLNode"] | None = None,
+        props: dict | None = None,
+    ):
+        self.tag = tag
+        self.value = value
+        self.children = children if children is not None else []
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self) -> str:
+        if self.props is None or self.props == "":
+            return ""
+
+        props = ""
+
+        for k in self.props:
+            props += f' {k}="{self.props[k]}"'
+
+        return props
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
